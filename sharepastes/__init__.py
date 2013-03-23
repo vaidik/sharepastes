5af3e9c8278548581d23fbd98fd407dd97bac6e5
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2013-03-23 15:52:36 +0530

    more info in __init__.py

--- sharepastes/__init__.py
+++ sharepastes/__init__.py
@@ -5,4 +5,6 @@ Helps you quickly share code snippets via various services like Github Gist and
 PasteBin.com.
 """
 
-__version__ = '0.1.0'
+__author__ = "Vaidik Kapoor <kapoor.vaidik@gmail.com>"
+__license__ = "MIT"
+__version__ = ".".join(map(str, (0, 1, 0)))
